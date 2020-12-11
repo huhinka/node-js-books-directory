4bcd5c78c6bdd900f395096dee92b4934e5b2541
huhinka <huhinka@163.com>
2020-12-11 15:47:55 +0800

    添加文件数据保存

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,17 @@
+const fs = require('fs/promises')
 const express = require('express')
 const router = express.Router()
 
-const books = [{ id: 1, name: 'Foo' }]
-let maxId = 1
+const BOOKS_FILE = 'books.json'
+
+async function getBooks () {
+  const json = await fs.readFile(BOOKS_FILE, 'utf-8')
+  return JSON.parse(json)
+}
+
+async function setBooks (json) {
+  await fs.writeFile(BOOKS_FILE, JSON.stringify(json))
+}
 
 /**
  * getting all books
@@ -10,7 +19,8 @@ let maxId = 1
 router.get('/', async (req, res) => {
   // 获取 query 参数
   const name = req.query.name
-  const match = (name) ? books.filter((book) => book.name === name) : books
+  const existing = await getBooks()
+  const match = (name) ? existing.filter((book) => book.name === name) : existing
 
   res.send(match)
 })
@@ -18,12 +28,11 @@ router.get('/', async (req, res) => {
 /**
  * getting only one book by id
  */
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // 获取 path 参数
   const id = +req.params.id
-  const match = books.find((book) => book.id === id)
-
-  console.log(books)
+  const existing = await getBooks()
+  const match = existing.find((book) => book.id === id)
 
   res.send(match)
 })
@@ -31,11 +40,14 @@ router.get('/:id', (req, res) => {
 /**
  * add a new book to the list
  */
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // 获取 json body
   const book = req.body
-  book.id = ++maxId
-  books.push(book)
+  const existing = await getBooks()
+  const maxId = Math.max.apply(null, existing.map((book) => book.id))
+  book.id = maxId + 1
+  existing.push(book)
+  await setBooks(existing)
 
   res.send(`Got a book ${JSON.stringify(book)}`)
 })
@@ -43,30 +55,35 @@ router.post('/', (req, res) => {
 /**
  * updating the existing book
  */
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
   const book = req.body
-  const existing = books.find((existing) => book.id === existing.id)
 
-  if (existing === null) {
-    res.send('Not Found')
-  } else {
-    Object.assign(existing, book)
+  const existing = await getBooks()
+  const found = existing.find((existing) => book.id === existing.id)
+
+  if (found) {
+    Object.assign(found, book)
+    await setBooks(existing)
 
     res.send(`Update book ${JSON.stringify(book)}`)
+  } else {
+    res.send('Not Found')
   }
 })
 
 /**
  * remove the book from the list
  */
-router.delete('/:id', (req, res) => {
-  const index = books.findIndex((book) => book.id === +req.params.id)
+router.delete('/:id', async (req, res) => {
+  const existing = await getBooks()
+  const index = existing.findIndex((book) => book.id === +req.params.id)
 
   if (index === -1) {
     res.send('Not Found')
   } else {
-    const deleted = books[index]
-    books.splice(index, 1)
+    const deleted = existing[index]
+    existing.splice(index, 1)
+    await setBooks(existing)
 
     res.send(`Deleted book ${JSON.stringify(deleted)}`)
   }
